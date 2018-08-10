3a2c60fbaaa70d6378ba2a56c766bafc64a233c9
Noa <brian.haslop@mercadolibre.com>
2018-08-10 13:18:28 -0300

    mend

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,6 +1,6 @@
 package app
 
-import "github.com/bhaslop/bolaso/apptrollers"
+import "github.com/bhaslop/bolaso/app/controllers"
 
 func mapUrlsToControllers() {
 	router.GET("/", controllers.GetHome)
